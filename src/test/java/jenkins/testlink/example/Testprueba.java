d15c0d2ae044bf1b3853a67761ad3f099d4a6bd0
Herrera Garay <KHerrera@bnb.com.bo>
2020-01-21 16:11:53 -0400

    segundo commit

---
+++ src/test/java/jenkins/testlink/example/Testprueba.java
@@ -0,0 +1,31 @@
+package jenkins.testlink.example;
+
+
+
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Testprueba {
+	protected int value1, value2;
+
+	// assigning the values
+	@Before
+	protected void setUp() {
+		value1 = 3;
+		value2 = 4;
+	}
+
+	// test method to add two values
+	@Test
+	public void testAdd() {
+		System.out.println("***********************testAdd*************************");
+		int result = value1 + value2;
+	
+		
+	}
+}
